1e614d5700b231b247b26a1f3b80628b3625f7d0
Tamizh <tamizh8469@gmail.com>
2025-02-19 18:29:45 +0530

    commit

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -12,23 +12,23 @@ export default function Home({ profiles }) {
       <div className="row">
         {profiles.map((profile) => (
           <div key={profile.id} className="col-12 col-sm-6 col-md-3">
-            <Link
-              to={`/profile/${profile.id}`}
-              className="text-decoration-none"
-            >
-              <div className="card" style={{ width: "18rem" }}>
-                <img
-                  src={profile.image}
-                  className="card-img-top"
-                  alt={profile.name}
-                />
-                <div className="card-body">
-                  <h5 className="card-title">{profile.name}</h5>
-                  <p className="card-text">Reg No: {profile.regNo}</p>
+            <div className="card" style={{ width: "18rem" }}>
+              <img
+                src={profile.image}
+                className="card-img-top"
+                alt={profile.name}
+              />
+              <div className="card-body">
+                <h5 className="card-title">{profile.name}</h5>
+                <p className="card-text">Reg No: {profile.regNo}</p>
+                <Link
+                  to={`/profile/${profile.id}`}
+                  className="text-decoration-none"
+                >
                   <Button className="btn btn-primary">View Profile</Button>
-                </div>
+                </Link>
               </div>
-            </Link>
+            </div>
           </div>
         ))}
       </div>
